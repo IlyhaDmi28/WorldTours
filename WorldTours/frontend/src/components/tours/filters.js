257dc1fe46586962bad0da051e51ff14a7e79451
Ilyha <mondaxfeall1@gmail.com>
2024-10-31 11:47:59 +0300

    update filters

--- WorldTours/frontend/src/components/tours/filters.js
+++ WorldTours/frontend/src/components/tours/filters.js
@@ -1,7 +1,31 @@
+import React, { useState } from 'react';
 import star from '../../img/star.svg'
+import darkStar from '../../img/dark-star.svg'
 import close from '../../img/close.svg'
 
 function Filters({ isFiltersOpen, closeFilters }) {
+    const [filters, setFilters] = useState({
+        minPrice: 0,
+        maxPrice: 99999,
+        minHotelStars: 1,
+        maxHotelStars: 5,
+        nutritionType: 'Не важно',
+        wifi: 'no_preference',
+        beach: 'no_preference',
+        separateBeds: 'no_preference',
+        separateBathroom: 'no_preference',
+        pool: 'no_preference',
+        jacuzzi: 'no_preference',
+        disco: 'no_preference',
+        billiards: 'no_preference',
+        tableTennis: 'no_preference',
+    });
+
+
+    const [minNumberLightHotelStars, setMinNumberLightHotelStars] = useState(filters.minHotelStars);
+    const [maxNumberLightHotelStars, setMaxNumberLightHotelStars] = useState(filters.maxHotelStars);
+   
+
     if (!isFiltersOpen) {
         return null; // Если модальное окно закрыто, возвращаем null, чтобы не рендерить его
     }
@@ -10,6 +34,52 @@ function Filters({ isFiltersOpen, closeFilters }) {
         if (e.target === e.currentTarget) {
             closeFilters();
         }
+    };
+
+
+    const ClickMinHotelStars = (hotelStarButtonId) => {
+        setMinNumberLightHotelStars(hotelStarButtonId);
+        setFilters((prevfilters) => ({
+            ...prevfilters,
+            ["minHotelStars"]: hotelStarButtonId,
+        }));
+    }
+
+    const ClickMaxHotelStars = (hotelStarButtonId) => {
+        setMaxNumberLightHotelStars(hotelStarButtonId);
+        setFilters((prevfilters) => ({
+            ...prevfilters,
+            ["maxHotelStars"]: hotelStarButtonId,
+        }));
+    }
+
+    const changeFilters = (e) => {
+        const { name, value } = e.target;
+        setFilters((prevfilters) => ({
+            ...prevfilters,
+            [name]: value,
+        }));
+    };
+
+    const clearAllFilters = () => {
+        setFilters({
+            minPrice: '',
+            maxPrice: '',
+            minHotelStars: 1,
+            maxHotelStars: 5,
+            nutritionType: 'Не важно',
+            wifi: 'no_preference',
+            beach: 'no_preference',
+            separateBeds: 'no_preference',
+            separateBathroom: 'no_preference',
+            pool: 'no_preference',
+            jacuzzi: 'no_preference',
+            disco: 'no_preference',
+            billiards: 'no_preference',
+            tableTennis: 'no_preference',
+        });
+        setMinNumberLightHotelStars(1);
+        setMaxNumberLightHotelStars(5);
     };
 
     return (
@@ -23,11 +93,11 @@ function Filters({ isFiltersOpen, closeFilters }) {
                     <div className="max-min-price-filter">
                         <div>
                             <div>Мин. цена</div>
-                            <input type="number"/>
+                            <input type="number" name="minPrice" value={filters.minPrice}  placeholder="От" onChange={changeFilters}/>
                         </div>
                         <div>
                             <div>Макс. цена</div>
-                            <input type="number"/>
+                            <input type="number" name="maxPrice" value={filters.maxPrice} placeholder="До" onChange={changeFilters}/>
                         </div>
                     </div>
 
@@ -35,22 +105,40 @@ function Filters({ isFiltersOpen, closeFilters }) {
                         <div>
                             <span>Мин. звёзды отеля:</span>
                             <div className='hotel-stars-buttons'>
-                                <button>
+                                <button
+                                    onMouseEnter={() => setMinNumberLightHotelStars(1)}
+                                    onMouseLeave={() => setMinNumberLightHotelStars(filters.minHotelStars)}
+                                    onClick={() => ClickMinHotelStars(1)}
+                                >
+                                    <img src={minNumberLightHotelStars >= 1 ? star : darkStar}/>
                                 </button>
-                                <button>
-                                    <img src={star}/>
+                                <button
+                                    onMouseEnter={() => setMinNumberLightHotelStars(2)}
+                                    onMouseLeave={() => setMinNumberLightHotelStars(filters.minHotelStars)}
+                                    onClick={() => ClickMinHotelStars(2)}
+                                >
+                                    <img src={minNumberLightHotelStars >= 2 ? star : darkStar}/>
                                 </button>
-                                <button>
-                                    <img src={star}/>
+                                <button
+                                    onMouseEnter={() => setMinNumberLightHotelStars(3)}
+                                    onMouseLeave={() => setMinNumberLightHotelStars(filters.minHotelStars)}
+                                    onClick={() => ClickMinHotelStars(3)}
+                                >
+                                    <img src={minNumberLightHotelStars >= 3 ? star : darkStar}/>
                                 </button>
-                                <button>
-                                    <img src={star}/>
+                                <button
+                                    onMouseEnter={() => setMinNumberLightHotelStars(4)}
+                                    onMouseLeave={() => setMinNumberLightHotelStars(filters.minHotelStars)}
+                                    onClick={() => ClickMinHotelStars(4)}
+                                >
+                                    <img src={minNumberLightHotelStars >= 4 ? star : darkStar}/>
                                 </button>
-                                <button>
-                                    <img src={star}/>
-                                </button>
-                                <button>
-                                    <img src={star}/>
+                                <button
+                                    onMouseEnter={() => setMinNumberLightHotelStars(5)}
+                                    onMouseLeave={() => setMinNumberLightHotelStars(filters.minHotelStars)}
+                                    onClick={() => ClickMinHotelStars(5)}
+                                >
+                                    <img src={minNumberLightHotelStars >= 5 ? star : darkStar}/>
                                 </button>
                             </div>
                         </div>
@@ -58,22 +146,40 @@ function Filters({ isFiltersOpen, closeFilters }) {
                         <div>
                             <span>Макс. звёзды отеля:</span>
                             <div className='hotel-stars-buttons'>
-                                <button>
+                                <button
+                                    onMouseEnter={() => setMaxNumberLightHotelStars(1)}
+                                    onMouseLeave={() => setMaxNumberLightHotelStars(filters.maxHotelStars)}
+                                    onClick={() => ClickMaxHotelStars(1)}
+                                >
+                                    <img src={maxNumberLightHotelStars >= 1 ? star : darkStar}/>
                                 </button>
-                                <button>
-                                    <img src={star}/>
+                                <button
+                                    onMouseEnter={() => setMaxNumberLightHotelStars(2)}
+                                    onMouseLeave={() => setMaxNumberLightHotelStars(filters.maxHotelStars)}
+                                    onClick={() => ClickMaxHotelStars(2)}
+                                >
+                                    <img src={maxNumberLightHotelStars >= 2 ? star : darkStar}/>
                                 </button>
-                                <button>
-                                    <img src={star}/>
+                                <button
+                                    onMouseEnter={() => setMaxNumberLightHotelStars(3)}
+                                    onMouseLeave={() => setMaxNumberLightHotelStars(filters.maxHotelStars)}
+                                    onClick={() => ClickMaxHotelStars(3)}
+                                >
+                                    <img src={maxNumberLightHotelStars >= 3 ? star : darkStar}/>
                                 </button>
-                                <button>
-                                    <img src={star}/>
+                                <button
+                                    onMouseEnter={() => setMaxNumberLightHotelStars(4)}
+                                    onMouseLeave={() => setMaxNumberLightHotelStars(filters.maxHotelStars)}
+                                    onClick={() => ClickMaxHotelStars(4)}
+                                >
+                                    <img src={maxNumberLightHotelStars >= 4 ? star : darkStar}/>
                                 </button>
-                                <button>
-                                    <img src={star}/>
-                                </button>
-                                <button>
-                                    <img src={star}/>
+                                <button
+                                    onMouseEnter={() => setMaxNumberLightHotelStars(5)}
+                                    onMouseLeave={() => setMaxNumberLightHotelStars(filters.maxHotelStars)}
+                                    onClick={() => ClickMaxHotelStars(5)}
+                                >
+                                    <img src={maxNumberLightHotelStars >= 5 ? star : darkStar}/>
                                 </button>
                             </div>
                         </div>
@@ -81,7 +187,7 @@ function Filters({ isFiltersOpen, closeFilters }) {
 
                     <div className="nutrition-filter">
                         <div>Питание: </div>
-                        <select>
+                        <select name="nutritionType" value={filters.nutritionType} onChange={changeFilters}>
                             <option selected="selected">Не важно</option>
                             <option>RO (Room only) — без питания</option>
                             <option>BB (Bed & breakfast) — завтрак</option>
@@ -102,56 +208,55 @@ function Filters({ isFiltersOpen, closeFilters }) {
                     <div class="heading-filters">Нет</div>
 
                     <div class="characteristic-filter">Наличие Wi-fi:</div>
-                    <div><input type="radio" name="wifi" checked/></div>
-                    <div><input type="radio" name="wifi"/></div>
-                    <div><input type="radio" name="wifi"/></div>
+                    <div><input type="radio" name="wifi" value="no_preference" checked={filters['wifi'] === 'no_preference'}  onChange={changeFilters}/></div>
+                    <div><input type="radio" name="wifi" value="yes" checked={filters['wifi'] === 'yes'}  onChange={changeFilters}/></div>
+                    <div><input type="radio" name="wifi" value="no" checked={filters['wifi'] === 'no'}  onChange={changeFilters}/></div>
 
                     <div class="characteristic-filter">Пляж рядом:</div>
-                    <div><input type="radio" name="beach" checked/></div>
-                    <div><input type="radio" name="beach"/></div>
-                    <div><input type="radio" name="beach"/></div>
+                    <div><input type="radio" name="beach" value="no_preference" checked={filters['beach'] === 'no_preference'}  onChange={changeFilters}/></div>
+                    <div><input type="radio" name="beach" value="yes" checked={filters['beach'] === 'yes'}  onChange={changeFilters}/></div>
+                    <div><input type="radio" name="beach" value="no" checked={filters['beach'] === 'no'}  onChange={changeFilters}/></div>
 
                     <div class="characteristic-filter">Раздельные кровати:</div>
-                    <div><input type="radio" name="separate_beds" checked/></div>
-                    <div><input type="radio" name="separate_beds"/></div>
-                    <div><input type="radio" name="separate_beds"/></div>
+                    <div><input type="radio" name="separateBeds" value="no_preference" checked={filters['separateBeds'] === 'no_preference'}  onChange={changeFilters}/></div>
+                    <div><input type="radio" name="separateBeds" value="yes" checked={filters['separateBeds'] === 'yes'}  onChange={changeFilters}/></div>
+                    <div><input type="radio" name="separateBeds" value="no" checked={filters['separateBeds'] === 'no'}  onChange={changeFilters}/></div>
 
                     <div class="characteristic-filter">Раздельный санузел:</div>
-                    <div><input type="radio" name="separate_bathroom" checked/></div>
-                    <div><input type="radio" name="separate_bathroom"/></div>
-                    <div><input type="radio" name="separate_bathroom"/></div>
+                    <div><input type="radio" name="separateBathroom" value="no_preference" checked={filters['separateBathroom'] === 'no_preference'}  onChange={changeFilters}/></div>
+                    <div><input type="radio" name="separateBathroom" value="yes" checked={filters['separateBathroom'] === 'yes'}  onChange={changeFilters}/></div>
+                    <div><input type="radio" name="separateBathroom" value="no" checked={filters['separateBathroom'] === 'no'}  onChange={changeFilters}/></div>
 
                     <div class="characteristic-filter">Бассейн:</div>
-                    <div><input type="radio" name="pool" checked/></div>
-                    <div><input type="radio" name="pool"/></div>
-                    <div><input type="radio" name="pool"/></div>
+                    <div><input type="radio" name="pool" value="no_preference" checked={filters['pool'] === 'no_preference'}  onChange={changeFilters}/></div>
+                    <div><input type="radio" name="pool" value="yes" checked={filters['pool'] === 'yes'}  onChange={changeFilters}/></div>
+                    <div><input type="radio" name="pool" value="no" checked={filters['pool'] === 'no'}  onChange={changeFilters}/></div>
 
                     <div class="characteristic-filter">Джакузи:</div>
-                    <div><input type="radio" name="jacuzzi" checked/></div>
-                    <div><input type="radio" name="jacuzzi"/></div>
-                    <div><input type="radio" name="jacuzzi"/></div>
+                    <div><input type="radio" name="jacuzzi" value="no_preference" checked={filters['jacuzzi'] === 'no_preference'}  onChange={changeFilters}/></div>
+                    <div><input type="radio" name="jacuzzi" value="yes" checked={filters['jacuzzi'] === 'yes'}  onChange={changeFilters}/></div>
+                    <div><input type="radio" name="jacuzzi" value="no" checked={filters['jacuzzi'] === 'no'}  onChange={changeFilters}/></div>
 
                     <div class="characteristic-filter">Дискотеки:</div>
-                    <div><input type="radio" name="disco" checked/></div>
-                    <div><input type="radio" name="disco"/></div>
-                    <div><input type="radio" name="disco"/></div>
+                    <div><input type="radio" name="disco" value="no_preference" checked={filters['disco'] === 'no_preference'}  onChange={changeFilters}/></div>
+                    <div><input type="radio" name="disco" value="yes" checked={filters['disco'] === 'yes'}  onChange={changeFilters} /></div>
+                    <div><input type="radio" name="disco" value="no" checked={filters['disco'] === 'no'}  onChange={changeFilters}/></div>
 
                     <div class="characteristic-filter">Бильярд:</div>
-                    <div><input type="radio" name="billiards" checked/></div>
-                    <div><input type="radio" name="billiards"/></div>
-                    <div><input type="radio" name="billiards"/></div>
+                    <div><input type="radio" name="billiards" value="no_preference" checked={filters['billiards'] === 'no_preference'}  onChange={changeFilters}/></div>
+                    <div><input type="radio" name="billiards" value="yes" checked={filters['billiards'] === 'yes'}  onChange={changeFilters}/></div>
+                    <div><input type="radio" name="billiards" value="no" checked={filters['billiards'] === 'no'}  onChange={changeFilters}/></div>
 
                     <div class="characteristic-filter">Тенис (настольный):</div>
-                    <div><input type="radio" name="table_tennis" checked/></div>
-                    <div><input type="radio" name="table_tennis"/></div>
-                    <div><input type="radio" name="table_tennis"/></div>
+                    <div><input type="radio" name="tableTennis" value="no_preference" checked={filters['tableTennis'] === 'no_preference'}  onChange={changeFilters}/></div>
+                    <div><input type="radio" name="tableTennis" value="yes" checked={filters['tableTennis'] === 'yes'}  onChange={changeFilters}/></div>
+                    <div><input type="radio" name="tableTennis" value="no" checked={filters['tableTennis'] === 'no'}  onChange={changeFilters}/></div>
                 </div>
 
                 <hr></hr>
                 <div className="filter-controllers">
-                    <button>Очистить всё</button>
+                    <button onClick={clearAllFilters}>Очистить всё</button>
                     <button>Показать</button>
-
                 </div>
             </div>
         </div>
